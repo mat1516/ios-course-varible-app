f4a93e46f1c5d84aa66fb3501362cc403398af24
Abdul Khan <mateen_matrix@hotmail.com>
2015-12-29 20:10:10 +0000

    These are the new variables

---
+++ variable_exercise.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var num = 15
+var num2 = 5
+var sum = num + num2
+print(sum)
+var mult = num2 * 9
+print(mult)
+var minus = num - num2
+print(minus)
+var div = num / num2
+print(div)
+
+var a = 10, b = 60, c = 25 // variable is for the changes that variate
+let a1 = 20 , b1 = 30, c1 = 75 // let is for the constant
+
+var h : Double
+var k : Int = 423
+var string1 : String = "Hello"
+var string2 : String = "World"
+var string3 : String
+string3 = string1 + string2
+
+print(string3)
+
